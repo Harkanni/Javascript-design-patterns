f0ee545589c8de8ab3227aad33050387a1df8138
harkanni <harkanzyempire001@gmail.com>
2023-05-23 23:59:32 +0100

    singleton design

--- Design Patterns/FactoryDesignPattern.js
+++ Design Patterns/FactoryDesignPattern.js
@@ -1,2 +1,3 @@
-// Factory design patterns are used when we want to create objects that have some similar properties in common. For example an automobile may have some different types of vehicles e.g, Ford, SUV, Tesla, Motorcycle, Bicycle, etc which have similar functions like Move, Park, etc. They could create a single Factory class or Object that creates instances of these various automobiles.
+// Factory design patterns are used when we want to create objects that have some similar properties in common. 
+// For example an automobile may have some different types of vehicles e.g, Ford, SUV, Tesla, Motorcycle, Bicycle, etc which have similar functions like Move, Park, etc. They could create a single Factory class or Object that creates instances of these various automobiles.
 // Factory designs can be create in two ways
