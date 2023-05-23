8b43ddb32125061ca1d09832f9ef4621c7fdaefa
harkanni <harkanzyempire001@gmail.com>
2023-05-23 23:26:08 +0100

    initial commit

---
+++ Design Patterns/SingletonDesignPattern.js
@@ -0,0 +1,5 @@
+// SINGLETON DESIGN PATTERN
+// The singleton design pattern is a design pattern mostly used in cases where we need a single point of truth in our application. This design creates immutable instances of objects in our application.
+// It could be created in two ways.
+    // 1. Using Object literals
+    // 2. Using Javascript classes.
