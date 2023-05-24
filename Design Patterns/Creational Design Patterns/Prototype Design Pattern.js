d31e65502369bec0d7e35c15ad3fd4a20828ebf7
harkanni <harkanzyempire001@gmail.com>
2023-05-24 03:29:50 +0100

    Adapter design pattern

--- Design Patterns/Creational Design Patterns/Prototype Design Pattern.js
+++ Design Patterns/Creational Design Patterns/Prototype Design Pattern.js
@@ -1,11 +1,13 @@
 // This design pattern is used to create new objects from other objects so that the new object inherits the properties and methods of the previous object. This is the concept of prototypal inheritance
 // 
 const Player = {
-    greet: () => console.log(this.name, 'says goodmorning'),
+    greet: function() {console.log(this.name, 'says Good morning')},
     health: 100,
 }
 const wizard = {
     name: "WIzard of Oz",
     attack: "Magic wand"
 }
-wizard.prototype = Player;
+Object.setPrototypeOf(wizard, Player)
+wizard.greet() // => Wizard of Oz says Good morning
+wizard.health = 100
